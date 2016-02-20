06a1fa19dd1d5746419094e94068d78830ada8a6
Jeff Weiler <jeffweiler@outlook.com>
2016-02-20 09:08:36 -0500

    first commit

---
+++ AsyncAwaitBlog1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncAwaitBlog1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Text = "";
+            var content = await AccessTheWebAsync();
+            resultsTextBox.Text = content;
+
+        }
+
+        public async Task<string> AccessTheWebAsync()
+        {
+            var httpClient = new HttpClient();
+            var content = await httpClient.GetStringAsync("http://msdn.microsoft.com");
+            return content;
+        }
+
+        private void SyncButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultsTextBox.Text = "";
+            var content = AccessTheWeb();
+            resultsTextBox.Text = content;
+        }
+
+        public string AccessTheWeb()
+        {
+            var httpClient = new WebClient();
+            var content = httpClient.DownloadString("http://msdn.microsoft.com");
+            return content;
+        }
+    }
+}
